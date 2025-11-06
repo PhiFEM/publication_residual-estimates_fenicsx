860967ee1555d3d1a7117fadb694bd34f98264bf
rbulle <rbulle@tutanota.com>
2025-11-06 07:59:48 +0100

    Change parameters test cases.

--- demo/plot.py
+++ demo/plot.py
@@ -44,7 +44,7 @@ for parameter in parameters_list:
     if ref_type == "unif":
         trunc = -3
     elif ref_type == "adap":
-        trunc = -10
+        trunc = -8
     data_path = os.path.join(
         demo,
         "output_" + param_name,
